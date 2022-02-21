6d91bb04065a00de05f3c08e5d96f866e92579e4
Nam Anh Hua <xpandi19@gmail.com>
2022-02-21 19:46:28 +0100

    Modified App.js for a better image upload solution.

--- deepface_ui_react/src/App.js
+++ deepface_ui_react/src/App.js
@@ -1,15 +1,36 @@
-function App() {
-  return (
-    <div>
-      <h1>Deep Cheap</h1>
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+
+    this.onImageChange = this.onImageChange.bind(this);
+  }
+
+  onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      this.setState({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
+
+  render() {
+    return (
       <div>
-        <h2>TITLE</h2>
         <div>
-          <button>Upload</button>
+          <div>
+            <img src={this.state.image} />
+            <h1>Select Image</h1>
+            <input type="file" name="myImage" onChange={this.onImageChange} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

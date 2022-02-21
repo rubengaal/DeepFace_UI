5c359a7a2c1ea3375b411b5e2b2a0c28bdabd509
Nam Anh Hua <xpandi19@gmail.com>
2022-02-21 19:02:34 +0100

    Uploading Demo react project

---
+++ deepface_ui_react/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Deep Cheap</h1>
+      <div>
+        <h2>TITLE</h2>
+        <div>
+          <button>Upload</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
